dc346362cd35699ba95174b80d8458e3ccdc3e20
Juan M. Rodríguez <juan.pj@gmail.com>
2021-01-25 15:24:52 -0600

    build: add pedantic for static analysis

--- lib/au10tix_flutter.dart
+++ lib/au10tix_flutter.dart
@@ -1,11 +1,9 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 
 class Au10tixFlutter {
-  static const MethodChannel _channel =
-      const MethodChannel('au10tix_flutter');
+  static const _channel = MethodChannel('au10tix_flutter');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
