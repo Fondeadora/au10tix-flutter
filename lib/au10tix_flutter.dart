d09ff4b15207295bbab7691fd6c554543bfe4ca1
Juan M. Rodríguez <juan.pj@gmail.com>
2021-02-02 14:03:32 -0600

    feat: define plugin interface

--- lib/au10tix_flutter.dart
+++ lib/au10tix_flutter.dart
@@ -2,11 +2,40 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
+enum DocumentType {
+  id,
+  passport,
+}
+
+class Au10tixResponse {
+  final DocumentType type;
+  final String documentFrontPath;
+  final String documentBackPath;
+  final String selfiePath;
+
+  Au10tixResponse(
+    this.type,
+    this.documentFrontPath,
+    this.documentBackPath,
+    this.selfiePath,
+  );
+}
+
 class Au10tixFlutter {
   static const _channel = MethodChannel('au10tix_flutter');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  static Future<Au10tixResponse> verifyId({String token}) async {
+    final args = {
+      'token': token,
+    };
+
+    await _channel.invokeMethod('verifyId', args);
+
+    return Au10tixResponse(
+      DocumentType.passport,
+      '',
+      '',
+      '',
+    );
   }
 }
--- test/au10tix_flutter_test.dart
+++ test/au10tix_flutter_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:au10tix_flutter/au10tix_flutter.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('au10tix_flutter');
@@ -15,9 +14,5 @@ void main() {
 
   tearDown(() {
     channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await Au10tixFlutter.platformVersion, '42');
   });
 }
